8bf0bf5e9a7e6d6f65195b9a80b851fe9606e8ee
Nolan Neeley <nolanneeley@gmail.com>
2018-01-15 22:44:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog 
   
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
-    
+    @breed = breed
   end 
   
   def name 
@@ -12,6 +12,13 @@ class Dog
   def name=(new_name)
     @name == new_name
   end
-
-
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed == new_breed
+  end
+  
 end
