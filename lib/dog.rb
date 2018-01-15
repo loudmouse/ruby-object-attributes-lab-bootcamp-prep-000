953476f85e744916e52763f54029b5a632a74d6e
Nolan Neeley <nolanneeley@gmail.com>
2018-01-15 22:48:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,20 +5,22 @@ class Dog
     @breed = breed
   end 
   
+  def name=(new_name)
+    @name == new_name
+  end
+  
   def name 
     @name
   end
   
-  def name=(new_name)
-    @name == new_name
+  def breed=(new_breed)
+    @breed == new_breed
   end
   
   def breed
     @breed
   end
   
-  def breed=(new_breed)
-    @breed == new_breed
-  end
+  
   
 end
